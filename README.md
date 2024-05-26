ba757461c0d61b3c6acb1e562e8ff4b1e31c1ad9
Franklin <egoistzty@gmail.com>
2024-05-26 17:53:23 +0800

    version for review

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-# sumcheck-on-G
-This is a modified sumcheck protocol for multilinear polynomials with group coefficients.
+# Compressed Sigma Protocol from Sumcheck
+This is a proof-of-concept implementation of a compressed sigma protocol from sumchecks.
 
-The project mainly refers to the HyperPlonk developed by Espresso System. 
+The protocol take k instances, each with m-length witnesses satisfying d-degree polynomil relation h.
 
-The fundamental multilinear polynomial interface is called from another repository https://github.com/FranklinZty/my-poly modified on ark-poly.
+## Sumcheck on Group
+The core building block for realizing such protocol is a modified sumcheck protocol for multilinear polynomials with group coefficients.
+This block mainly refers to the HyperPlonk developed by Espresso System and is built on top of the Arkworks libraries including ark-ff, ark-ec and ark-poly.
+
+## Amortization with Sumcheck on Field
+First, “Amortization” technique (implemented in sumcheck on field) is employed to randomly combine all high-degree instances into a folded one without introduing an extra linearization process.
+
+## Compression with Sumcheck on Group
+Second, a “compression” technique (implemented in sumcheck on group) is applied, which achieves a logarithmic communication overhead for the verification of the folded instance.
+
+## Building & Running
+As usual, you can run the project using `cargo run`.
--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,8 @@ use crate::{
 };
 use std::sync::Arc;
 use ark_poly::DenseMultilinearExtension;
-use ark_bls12_381::{Fr, Fq, G1Projective};
+use ark_test_curves::secp256k1::{Fr, G1Projective};
+// use ark_bls12_381::{Fr, Fq, G1Projective};
 use ark_std::{One, Zero, test_rng, UniformRand, time::Instant};
 use std::ops::Add;
 use std::marker::PhantomData;
@@ -28,10 +29,10 @@ use rand::Rng;
 
 // x, y dimensions M(x,y) - k times m matrix
 const num_x:usize = 8;
-const num_y:usize = 8;
+const num_y:usize = 4;
 // degrees for f(x) and g(y)
-const deg_x:usize = 1;
-const deg_y:usize = 2;
+const deg_x:usize = 2;
+const deg_y:usize = deg_x+1;
 fn main() {
     let base: usize = 2;
     let row_num = base.pow(u32::try_from(num_x).unwrap());
@@ -58,6 +59,12 @@ fn sumcheck_on_F(
     // further transform G_y_mle into a virtual polynomial
     let mut G_y_virtual =
     VirtualPolynomial::new_from_mle(&Arc::new(G_y_mle.clone()), Fr::one());
+    
+    let mut M_x_mle: DenseMultilinearExtension<Fr> = vec_to_mle(num_x, &vec![Fr::zero();  M.len()]);
+    let bhc = BooleanHypercube::new(num_y);
+    for y in bhc.into_iter() {
+        M_x_mle += fix_variables(&M_xy_mle, &y);
+    }
 
     let now = Instant::now();
     // ---------------- first round sum-check prover --------------------
@@ -77,8 +84,11 @@ fn sumcheck_on_F(
     }
 
     // transform GM(x) into a virtual polynomial on x
-    let GM_x_virtual =
+    let mut GM_x_virtual =
     VirtualPolynomial::new_from_mle(&Arc::new(sum_GM.clone()), Fr::one());
+    for _ in 0..deg_x-1 {
+        GM_x_virtual.mul_by_mle(Arc::new(M_x_mle.clone()), Fr::one()).unwrap();
+    }
 
     // initialize the transcript
     let mut transcript = <PolyIOP<Fr> as SumCheck<Fr>>::init_transcript();
@@ -96,6 +106,10 @@ fn sumcheck_on_F(
 
     let mut GM_y_virtual= M_y_virtual.clone();
     GM_y_virtual.mul_by_mle(Arc::new(G_y_mle.clone()), Fr::one()).unwrap();
+
+    for _ in 0..deg_x-1 {
+        GM_y_virtual.mul_by_mle(Arc::new(M_y_mle.clone()), Fr::one()).unwrap();
+    }
 
     let sumcheck_proof_y =
             <PolyIOP<Fr> as SumCheck<Fr>>::prove(&GM_y_virtual, & mut transcript).unwrap();
@@ -178,44 +192,53 @@ fn sumcheck_on_G(
     let mut G_y_virtual =
     VirtualPolynomial::new_from_mle(&Arc::new(G_y_mle.clone()), Fr::one());
 
+    let mut M_x_mle: DenseMultilinearExtension<Fr> = vec_to_mle(num_x, &vec![Fr::zero();  M.len()]);
+    let bhc = BooleanHypercube::new(num_y);
+    for y in bhc.into_iter() {
+        M_x_mle += fix_variables(&M_xy_mle, &y);
+    }
+
     let now = Instant::now();
-    // // ---------------- first round sum-check prover --------------------
-    // let mut sum_Mz = DenseMultilinearExtension::<Fr> {
-    //     evaluations: vec![Fr::zero(); M_xy_mle.evaluations.len()],
-    //     num_vars: num_x,
-    // };
-    // let bhc = BooleanHypercube::new(num_y);
-    // for y in bhc.into_iter() {
-    //     // In a slightly counter-intuitive fashion fix_variables() fixes the right-most variables of the polynomial. So
-    //     // for a polynomial M(x,y) and a random field element r, if we do fix_variables(M,r) we will get M(x,r).
-    //     let M_j_y = fix_variables(&M_xy_mle, &y);
-    //     let G_y = G_y_virtual.evaluate(&y).unwrap();
-    //     let M_j_z = scalar_mul(&M_j_y, &G_y);
-    //     sum_Mz = sum_Mz.add(M_j_z);
-    // }
+    // ---------------- first round sum-check prover --------------------
+    let mut sum_Mz = DenseMultilinearExtension::<Fr> {
+        evaluations: vec![Fr::zero(); M_xy_mle.evaluations.len()],
+        num_vars: num_x,
+    };
+    let bhc = BooleanHypercube::new(num_y);
+    for y in bhc.into_iter() {
+        // In a slightly counter-intuitive fashion fix_variables() fixes the right-most variables of the polynomial. So
+        // for a polynomial M(x,y) and a random field element r, if we do fix_variables(M,r) we will get M(x,r).
+        let M_j_y = fix_variables(&M_xy_mle, &y);
+        let G_y = G_y_virtual.evaluate(&y).unwrap();
+        let M_j_z = scalar_mul(&M_j_y, &G_y);
+        sum_Mz = sum_Mz.add(M_j_z);
+    }
 
-    // let mut GM_x_virtual =
-    // VirtualGroupPolynomial::new_from_mle(&Arc::new(sum_Mz.clone()), g);
+    let mut GM_x_virtual =
+    VirtualGroupPolynomial::new_from_mle(&Arc::new(sum_Mz.clone()), g);
+    for _ in 0..deg_x-1 {
+        GM_x_virtual.mul_by_mle(Arc::new(M_x_mle.clone()), Fr::one()).unwrap();
+    }
 
-    // let mut transcript = <PolyIOP<Fr> as SumCheck<Fr>>::init_transcript();
+    let mut transcript = <PolyIOP<Fr> as SumCheck<Fr>>::init_transcript();
 
-    // let sumcheck_proof_x =
-    //         <PolyIOP<Fr> as GroupSumCheck<G1Projective>>::prove(&GM_x_virtual, & mut transcript).unwrap();
-    // let r_x = sumcheck_proof_x.point.clone();
+    let sumcheck_proof_x =
+            <PolyIOP<Fr> as GroupSumCheck<G1Projective>>::prove(&GM_x_virtual, & mut transcript).unwrap();
+    let r_x = sumcheck_proof_x.point.clone();
         
     // ---------------- second round sum-check prover--------------------
     // compute GM(y) = M(rx,y) G(y)
-    let mut transcript = <PolyIOP<Fr> as SumCheck<Fr>>::init_transcript();
-    let r_raw: Vec<usize> = (0..num_y).map(|_| rng.gen_range(0..99)).collect();
-    let r_x = to_F_vec::<Fr>(r_raw);
     let M_y_mle = fix_variables(&M_xy_mle, &r_x);
     let M_y_virtual =
     VirtualGroupPolynomial::new_from_mle(&Arc::new(M_y_mle.clone()), g);
 
     let mut GM_y_virtual= M_y_virtual.clone();
     GM_y_virtual.mul_by_mle(Arc::new(G_y_mle.clone()), Fr::one()).unwrap();
+    for _ in 0..deg_x-1 {
+        GM_y_virtual.mul_by_mle(Arc::new(M_y_mle.clone()), Fr::one()).unwrap();
+    }
     let eval = GM_y_virtual.evaluate(&r_x);
-    
+
     let sumcheck_proof_y =
             <PolyIOP<Fr> as GroupSumCheck<G1Projective>>::prove(&GM_y_virtual, & mut transcript).unwrap();
     let r_y = sumcheck_proof_y.point.clone();
@@ -224,15 +247,15 @@ fn sumcheck_on_G(
     let elapsed_time = now.elapsed();
     println!("Sumcheck on G Prover time: {:?}", elapsed_time);
     
-    // // compute sum_x
-    // let mut sum_x = G1Projective::zero();
-    // let bhc = BooleanHypercube::new(num_x);
-    // for x in bhc.into_iter() {
-    //     // In a slightly counter-intuitive fashion fix_variables() fixes the right-most variables of the polynomial. So
-    //     // for a polynomial M(x,y) and a random field element r, if we do fix_variables(M,r) we will get M(x,r).
-    //     let GM_j_x = GM_x_virtual.evaluate(&x).unwrap();
-    //     sum_x += GM_j_x;
-    // }
+    // compute sum_x
+    let mut sum_x = G1Projective::zero();
+    let bhc = BooleanHypercube::new(num_x);
+    for x in bhc.into_iter() {
+        // In a slightly counter-intuitive fashion fix_variables() fixes the right-most variables of the polynomial. So
+        // for a polynomial M(x,y) and a random field element r, if we do fix_variables(M,r) we will get M(x,r).
+        let GM_j_x = GM_x_virtual.evaluate(&x).unwrap();
+        sum_x += GM_j_x;
+    }
 
     // compute sum_y
     let mut sum_y = G1Projective::zero();
@@ -254,14 +277,14 @@ fn sumcheck_on_G(
     
     let mut transcript = <PolyIOP<Fr> as GroupSumCheck<G1Projective>>::init_transcript();
 
-    // // ---------------- first round sum-check verifier--------------------
-    // let sumcheck_subclaim = <PolyIOP<Fr> as GroupSumCheck<G1Projective>>::verify(
-    //     sum_x,
-    //     &sumcheck_proof_x,
-    //     &vp_aux_info_x,
-    //     & mut transcript,
-    // )
-    // .unwrap();
+    // ---------------- first round sum-check verifier--------------------
+    let sumcheck_subclaim = <PolyIOP<Fr> as GroupSumCheck<G1Projective>>::verify(
+        sum_x,
+        &sumcheck_proof_x,
+        &vp_aux_info_x,
+        & mut transcript,
+    )
+    .unwrap();
 
     // ---------------- second round sum-check verifier --------------------
     let vp_aux_info_y = VGPAuxInfo::<G1Projective> {
